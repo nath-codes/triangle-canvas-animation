23ae0d98bb743368e08b3a7133b12fc24c7fed52
Nathan Tredgett <nathantredgett@gmail.com>
2018-08-23 14:17:42 +0100

    updated color scheme

--- modules/canvas.js
+++ modules/canvas.js
@@ -1,30 +1,30 @@
 import Triangles from "./triangles.js";
 
 class Canvas {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.canvas.style.background = "black";
-        this.context = this.canvas.getContext("2d");
-        this.addEventListener();
-        this.setCanvasSize();
-    }
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.canvas.style.background = "rgb(26, 14, 42)";
+    this.context = this.canvas.getContext("2d");
+    this.addEventListener();
+    this.setCanvasSize();
+  }
 
-    setCanvasSize() {
-        this.canvas.height = window.innerHeight;
-        this.canvas.width = window.innerWidth;
-    }
+  setCanvasSize() {
+    this.canvas.height = window.innerHeight;
+    this.canvas.width = window.innerWidth;
+  }
 
-    addEventListener() {
-        window.addEventListener("resize", () => {
-            this.setCanvasSize(this.canvas);
-            this.draw();
-        });
-    }
+  addEventListener() {
+    window.addEventListener("resize", () => {
+      this.setCanvasSize(this.canvas);
+      this.draw();
+    });
+  }
 
-    draw() {
-        const triangles = new Triangles(this.context, 10);
-        triangles.animate();
-    }
+  draw() {
+    const triangles = new Triangles(this.context, 10);
+    triangles.animate();
+  }
 }
 
 export default Canvas;
--- modules/triangle.js
+++ modules/triangle.js
@@ -1,53 +1,52 @@
 class Triangle {
-    constructor(context, width, spacer, isStatic) {
-        this.context = context;
-        this.isStatic = isStatic;
-        this.lineWidth = 5;
-        this.maxWidth = this.context.canvas.height;
-        this.originalWidth = width;
-        this.spacer = spacer;
-        this.strokeStyle = "white";
-        this.width = width;
-    }
+  constructor(context, width, spacer, isStatic) {
+    this.context = context;
+    this.isStatic = isStatic;
+    this.lineWidth = 5;
+    this.maxWidth = this.context.canvas.height;
+    this.originalWidth = width;
+    this.spacer = spacer;
+    this.strokeStyle = "rgba(212, 72, 139)";
+    this.width = width;
+  }
 
-    draw() {
-        if (!this.isStatic) {
-            this.update();
-        }
-        const horizontalCenter = this.context.canvas.width / 2;
-        const halfTriangleWidth = this.width / 2;
-        const verticalCenter = this.context.canvas.height / 2 + this.spacer;
-
-        this.context.beginPath();
-        this.context.moveTo(
-            horizontalCenter,
-            verticalCenter - halfTriangleWidth
-        );
-        this.context.lineTo(
-            horizontalCenter + halfTriangleWidth,
-            verticalCenter + halfTriangleWidth / 2
-        );
-        this.context.lineTo(
-            horizontalCenter - halfTriangleWidth,
-            verticalCenter + halfTriangleWidth / 2
-        );
-        this.context.lineTo(
-            horizontalCenter,
-            verticalCenter - halfTriangleWidth
-        );
-        this.context.strokeStyle = this.strokeStyle;
-        this.context.lineWidth = this.lineWidth;
-        this.context.closePath();
-        this.context.stroke();
+  draw() {
+    if (!this.isStatic) {
+      this.update();
     }
+    const horizontalCenter = this.context.canvas.width / 2;
+    const halfTriangleWidth = this.width / 2;
+    const verticalCenter = this.context.canvas.height / 2 + this.spacer;
 
-    update() {
-        if (this.width > this.maxWidth) {
-            this.width = 0;
-        } else {
-            this.width += 1;
-        }
+    this.context.beginPath();
+    this.context.moveTo(horizontalCenter, verticalCenter - halfTriangleWidth);
+    this.context.lineTo(
+      horizontalCenter + halfTriangleWidth,
+      verticalCenter + halfTriangleWidth / 2
+    );
+    this.context.lineTo(
+      horizontalCenter - halfTriangleWidth,
+      verticalCenter + halfTriangleWidth / 2
+    );
+    this.context.lineTo(horizontalCenter, verticalCenter - halfTriangleWidth);
+    this.context.strokeStyle = this.getStrokeStyle();
+    this.context.lineWidth = this.lineWidth;
+    this.context.closePath();
+    this.context.stroke();
+  }
+
+  getStrokeStyle() {
+    const opacity = this.isStatic ? 0.8 : (this.width / 1000).toFixed(1);
+    return `rgba(212, 72, 139, ${opacity})`;
+  }
+
+  update() {
+    if (this.width > this.maxWidth) {
+      this.width = 0;
+    } else {
+      this.width += 1;
     }
+  }
 }
 
 export default Triangle;
